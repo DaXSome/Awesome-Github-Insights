1209c9db38b3e766890731bf8775a23ce850b2db
owbird <owbirdphorccuo@gmail.com>
2024-02-29 14:57:12 +0000

    chore: Allow image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: [
+      "avatars.githubusercontent.com",
+      "github-profile-trophy.vercel.app",
+      "github-readme-stats.vercel.app",
+      "github-readme-streak-stats.herokuapp.com",
+      "github-readme-stats-git-masterrstaa-rickstaa.vercel.app",
+    ],
+  },
+};
 
 export default nextConfig;
