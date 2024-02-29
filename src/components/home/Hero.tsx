05fb4d698cf42da71c57e50cda644c6bc0fed2f9
owbird <owbirdphorccuo@gmail.com>
2024-02-29 15:31:43 +0000

    feat: Update hero

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -8,47 +8,40 @@ const Hero = ({ lastUpdate }: Props) => {
       <h1 className="text-4xl font-bold text-gray-800">Ghana Devs</h1>
 
       <p className="mt-4 text-lg text-gray-600">
-        We provide an index of Ghanaian developers with Github profile
-        statistics.
+        Welcome to Ghana Devs, your go-to resource for discovering Ghanaian
+        developers and exploring beautiful insights on their profiles.
       </p>
-      <p className="mt-2 text-gray-600">
-        The original index is based on the{" "}
+
+      <p className="mt-4 text-lg text-gray-600">
+        The original index is sourced from the{" "}
         <a
           href="https://github.com/gayanvoice/top-github-users/blob/main/markdown/total_contributions/ghana.md"
           className="text-blue-500 hover:text-blue-700 underline"
         >
           Top Github Users Repo
-        </a>
+        </a>{" "}
+        and is continuously updated to ensure accuracy.
       </p>
 
-      <p className="mt-4 text-gray-600">
-        You need at least{" "}
-        <i>
-          <span className="font-bold text-blue-500">14 followers</span>
-        </i>{" "}
-        and location set to{" "}
-        <i>
-          <span className="font-bold text-blue-500">Ghana</span>
-        </i>{" "}
-        to be found by the bot.
+      <p className="mt-4 text-lg text-gray-600">
+        To be discovered, developers must have at least{" "}
+        <span className="font-bold text-blue-500">14 followers</span> on GitHub
+        and have their location set to{" "}
+        <span className="font-bold text-blue-500">Ghana</span>. However, even if
+        you don&apos;t meet these criteria, you can still be found via a simple
+        username search.
       </p>
-      <p className="mt-2 text-gray-600">
-        However, a simple username search will still return the user profile
-        statistics if the user can&apos;t be found in the list.
-      </p>
-      <p className="mt-4 text-gray-600">
-        Kindly submit a{" "}
-        <i>
-          <span className="font-bold text-blue-500">PR</span>
-        </i>{" "}
-        at{" "}
+
+      <p className="mt-4 text-lg text-gray-600">
+        Can&apos;t find your name on the index? Don&apos;t worry! Simply submit
+        an <span className="font-bold text-blue-500">Issue</span> to{" "}
         <a
-          href="https://github.com/owbird/Ghana-Devs"
+          href="https://github.com/owbird/Ghana-Devs/issues/new"
           className="text-blue-500 hover:text-blue-700 underline"
         >
           Ghana-Devs
         </a>{" "}
-        if you can&apos;t find your name but want to be indexed.
+        to be included.
       </p>
 
       <h2 className="mt-8 text-xl font-medium text-gray-600">
